a41767b41972f9c58d7a15b4b67ca35ba1f19d60
Sandro Sousa Rivera <sandrosousarivera@sousas-P-machine.local>
2023-05-17 14:17:55 +0200

    adding responsive menu

--- app.js
+++ app.js
@@ -9,5 +9,14 @@ const observer = new IntersectionObserver((entries)=>{
     })
 });
 
+
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el)=> observer.observe(el))
+
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
